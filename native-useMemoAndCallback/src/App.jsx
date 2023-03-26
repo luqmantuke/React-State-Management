1682fb4a33f8deca5dc09444b75c54dbf0b78b0c
Luqman Tuke <lsuleiman2002@gmail.com>
2023-03-26 12:08:27 +0300

    see how many times it renders

--- native-useMemoAndCallback/src/App.jsx
+++ native-useMemoAndCallback/src/App.jsx
@@ -2,18 +2,27 @@ import { useState,useMemo } from 'react'
 
 import './App.css'
 
+function SortedList({names}){
+  const sortedNames = useMemo(() => {
+    [...names].sort()
+    console.log("sorted")
+  }, [names])
+  return       <div>SortedNames: {sortedNames.join(", ")}</div>
+
+}
+
 function App() {
 
   const [numbers, setnumbers] = useState([10,20,30])
   const total = useMemo(() => numbers.reduce((acc,number)=> acc+number,0), [numbers])
   const [names, setnames] = useState(['John', "Paul", "George", "Ringo"])
-  const sortedNames = useMemo(() => [...names].sort(), [names])
+
 
   return (
     <div className="App">
       <div>Total: {total}</div>
       <div>Names: {names.join(", ")}</div>
-      <div>SortedNames: {sortedNames.join(", ")}</div>
+      <SortedList names={names}/>
     </div>
   )
 }
