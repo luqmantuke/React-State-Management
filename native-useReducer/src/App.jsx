6e315744f43d3f2365fbfddee76bf1e8a99f7044
Luqman Tuke <lsuleiman2002@gmail.com>
2023-03-26 10:26:25 +0300

    useReducer setNames

---
+++ native-useReducer/src/App.jsx
@@ -0,0 +1,34 @@
+import { useReducer } from 'react'
+
+
+function App() {
+  const [state, dispatch] = useReducer((state,action)=>{
+    switch (action.type) {
+      case "SET_NAME":
+        
+        return {...state,name:action.payload};
+      case "ADD_NAME":
+        return {...state, names: [...state.names, state.name], name: ""}
+      default:
+        break;
+    }
+  },{
+    names: [],
+    name: ""
+  })
+  return (
+    <div >
+     <input type="text" value={state.name} onChange={(e) => dispatch({type: "SET_NAME", payload: e.target.value})} />
+     <div>Names: {state.names.map((name,index)=>{
+
+ <div key={index}>{name}</div>
+console.log(name)
+     }
+      )} </div>
+
+     <button onClick={()=> dispatch({type: "ADD_NAME",})}>Add Name</button>
+    </div>
+  )
+}
+
+export default App
---
+++ native-useReducer/src/useReducerfunction.ts
@@ -0,0 +1,3 @@
+const numbers = [10,20,30]
+
+numbers.reduce((cv,n)=>cv+n)
